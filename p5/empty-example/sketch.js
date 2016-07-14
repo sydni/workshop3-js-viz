db10cea5074c9ac8115dec3bf517063395f17e92
Sydni Topper <sydni.topper.18@dartmouth.edu>
2016-07-14 16:46:05 -0400

    p5 stuff

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  stroke(255,127,80);
+  fill(40,70,100);
 }
 
 function draw() {
+  ellipse(mouseX,mouseY,80,80);
 
 }
